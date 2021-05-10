7da826aabfa111cf92a89f914a83414e892b8c4d
Steven Barclay <stejbac@gmail.com>
2021-05-10 03:35:06 +0100

    Fix doc comment

--- finance/src/main/java/misq/finance/swap/contract/multiSig/MultiSigProtocol.java
+++ finance/src/main/java/misq/finance/swap/contract/multiSig/MultiSigProtocol.java
@@ -20,23 +20,26 @@ package misq.finance.swap.contract.multiSig;
 import misq.finance.contract.*;
 import misq.p2p.P2pService;
 import misq.p2p.node.MessageListener;
-/**
- * Mock protocol for simulating the a basic 2of2 Multisig protocol (MAD).
- * Maker is BTC buyer and taker, Taker is seller and maker. There might be differences for buyer/seller roles
- * which would lead to 4 protocol variations, but we keep it simple and consider there are only 2 roles.
- * <p>
- * 1. Maker sends tx inputs
- * 3. Maker receives signed 2of MS tx from Taker, signs it and broadcasts it. Wait for confirmation
- * 4. After Tx is confirmed she sends funds to Taker and sends Taker a message including her signature for the payout tx.
- * 6. After Maker has received Takers message and sees the payout tx in the network she has completed.
- */
 
 /**
- * Taker awaits Maker commitment
- * 2. Taker receives tx inputs of alice and creates 2of MS tx, signs it and send it back to Maker.
- * Wait for confirmation once Maker has broadcast tx
- * 5. After Taker has received Maker message, checks if he has received the funds and if so he signs the payout tx and
- * broadcasts it and sends Maker a message that the payout tx is broadcast.
+ * Mock protocol for simulating the a basic 2of2 Multisig protocol (MAD). Maker is BTC buyer, Taker is seller. There
+ * might be differences for buyer/seller roles which would lead to 4 protocol variations, but we keep it simple and
+ * consider there are only 2 roles.
+ * <ol>
+ *   <li value="1">Maker sends tx inputs.
+ *   <li value="3">Maker receives signed 2of MS tx from Taker, signs it and broadcasts it. Wait for confirmation.
+ *   <li value="4">After Tx is confirmed she sends funds to Taker and sends Taker a message including her signature for
+ *   the payout tx.
+ *   <li value="6">After Maker has received Takers message and sees the payout tx in the network she has completed.
+ * </ol>
+ * <p>
+ * Taker awaits Maker commitment.
+ * <ol>
+ *   <li value="2">Taker receives tx inputs of Maker and creates 2of MS tx, signs it and send it back to Maker. Wait for
+ *   confirmation once Maker has broadcast tx.
+ *   <li value="5">After Taker has received Maker message, checks if he has received the funds and if so he signs the
+ *   payout tx and broadcasts it and sends Maker a message that the payout tx is broadcast.
+ * </ol>
  */
 public abstract class MultiSigProtocol extends TwoPartyProtocol implements MessageListener {
 
