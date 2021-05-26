a6d874802cc1ab7f1a141fe27c85108cdb096e51
Steven Barclay <stejbac@gmail.com>
2021-05-26 17:46:34 +0100

    Add initial SharedState proxy construction
    Provide a 'factory.create()' method which uses 'java.lang.reflect.Proxy'
    to instantiate an annotated shared state interface. The proxy holds a
    map, of type 'Map<String, FieldVersions<Object>>', from property names
    to all the different versions of each initialised property. As a
    property can have multiple '@DependsOn' annotations and may be
    additionally supplied externally via an incoming message or during
    initialisation of the trade state, the framework will attempt to keep
    track of the different versions and their sources to make sure that they
    are all the same. For example, 'MultiSig.SharedState::unsignedDepositTx'
    includes the '@DependsOn' alternatives:
    
      {'makerDepositTxInputs', 'takerDepositTxInputs', 'escrowAddress'},
      {'takerSignedDepositTx'},
    
    so the unsigned deposit tx may be constructed either from the initial
    inputs or by stripping the signature from the signed deposit tx received
    from the taker. If these are not byte-for-byte the same, then something
    has gone wrong and the framework will be able to detect that (TODO).
    
    To the above end, add a 'FieldVersions' class to hold all the available
    property versions and their sources. Also add an 'IndexInjector' utility
    class to allow the framework to select the appropriate '@DependsOn'
    alternative to use, via the 'oneOf(..)' selector called by the default
    method of the corresponding property. The return value is then memoised.
    
    Also add rudimentary logic to fire events and send/receive messages
    from/to the proxy object. Use an eager strategy of messaging every field
    to each counterparty which has access to it, as soon as it is learned
    by the state owner (as long as the same field hasn't already been sent
    to or received from the given counterparty).

--- finance/src/main/java/misq/finance/contract/sharedState/SharedStateFactory.java
+++ finance/src/main/java/misq/finance/contract/sharedState/SharedStateFactory.java
@@ -3,11 +3,22 @@ package misq.finance.contract.sharedState;
 import com.google.common.base.Functions;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.*;
+import com.google.common.reflect.AbstractInvocationHandler;
+import com.google.common.reflect.Invokable;
+import com.google.common.reflect.Reflection;
+import com.google.common.reflect.TypeToken;
 import lombok.Getter;
 import misq.finance.contract.SecurityProvider;
 
+import javax.annotation.Nonnull;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 import static misq.finance.contract.sharedState.AccessCondition.atom;
@@ -187,6 +198,231 @@ public class SharedStateFactory<T extends SecurityProvider.SharedState> {
 
     @SafeVarargs
     public static <T> T oneOf(Supplier<T>... alternativeDerivations) {
-        throw new UnsupportedOperationException();
+        return alternativeDerivations[IndexInjector.getIndex()].get();
+    }
+
+    private static final Object configurationMonitor = new Object();
+    private static volatile boolean eventFiring;
+    private static volatile SharedStateFactory<?>.StateInvocationHandler handlerUnderConfiguration;
+    private static List<String> getterInvocations;
+
+    public static <T> When<T> when(T result) {
+        return new When<>();
+    }
+
+    public static class When<T> {
+        void thenReturn(T value) {
+            synchronized (configurationMonitor) {
+                var name = getterInvocations.get(getterInvocations.size() - 1);
+                var source = new FieldVersions.ExternalSource(handlerUnderConfiguration.owner);
+                handlerUnderConfiguration.putFieldIfAbsent(name, source, value);
+            }
+        }
+    }
+
+    static <T> void fireEvent(Supplier<T> methodReference, T value) {
+        SharedStateFactory<?>.StateInvocationHandler handler;
+        String event;
+        synchronized (configurationMonitor) {
+            try {
+                eventFiring = true;
+                getterInvocations = new ArrayList<>();
+                methodReference.get();
+                Preconditions.checkArgument(getterInvocations.size() == 1,
+                        "Event getter should be invoked exactly once but got invocations: %s", getterInvocations);
+                (handler = handlerUnderConfiguration).setEventValue((event = getterInvocations.get(0)), value);
+            } finally {
+                eventFiring = false;
+                handlerUnderConfiguration = null;
+                getterInvocations = null;
+            }
+        }
+        handler.updateDependentFields(Set.of(event));
+    }
+
+    public T create(String owner, Consumer<T> initializationBlock) {
+        Preconditions.checkArgument(parties.contains(owner), "Unknown party: %s", owner);
+
+        var handler = new StateInvocationHandler(owner);
+        var state = handler.proxy;
+        var fieldsToInitialize = Maps.filterValues(supplierMap, owner::equals).keySet();
+
+        synchronized (configurationMonitor) {
+            try {
+                handlerUnderConfiguration = handler;
+                getterInvocations = new ArrayList<>();
+                initializationBlock.accept(state);
+                Preconditions.checkArgument(handler.fields.keySet().equals(fieldsToInitialize),
+                        "Wrong fields initialized for '%s'. Expected '%s' but got '%s'",
+                        owner, fieldsToInitialize, handler.fields.keySet());
+                Preconditions.checkArgument(getterInvocations.size() == fieldsToInitialize.size(),
+                        "Expected %s getter invocations in initialization block but got %s",
+                        fieldsToInitialize.size(), getterInvocations.size());
+            } finally {
+                handlerUnderConfiguration = null;
+                getterInvocations = null;
+            }
+        }
+
+        handler.updateDependentFields(fieldsToInitialize);
+        return state;
+    }
+
+    @SuppressWarnings("UnstableApiUsage")
+    private static final TypeToken<String> PARTY_TYPE = TypeToken.of(String.class);
+    @SuppressWarnings("UnstableApiUsage")
+    private static final TypeToken<Map<String, ?>> MESSAGE_TYPE = new TypeToken<>() {
+    };
+
+    @SuppressWarnings("UnstableApiUsage")
+    private class StateInvocationHandler extends AbstractInvocationHandler {
+        private final String owner;
+        private final T proxy;
+        private final Map<String, FieldVersions<Object>> fields = new ConcurrentHashMap<>();
+        private final Set<String> pendingActions = ConcurrentHashMap.newKeySet();
+        private final SetMultimap<String, String> recipientMultimap = MultimapBuilder.hashKeys().hashSetValues().build();
+
+        private StateInvocationHandler(String owner) {
+            this.owner = owner;
+            proxy = Reflection.newProxy(clazz, this);
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        protected Object handleInvocation(@Nonnull Object proxy, @Nonnull Method method, @Nonnull Object[] args) throws Throwable {
+            Preconditions.checkArgument(this.proxy == proxy);
+            var name = method.getName();
+            if (args.length == 0) {
+                if (this == handlerUnderConfiguration || eventObserverMap.containsKey(name) && eventFiring) {
+                    synchronized (configurationMonitor) {
+                        if (this == handlerUnderConfiguration || eventObserverMap.containsKey(name) && eventFiring) {
+                            handlerUnderConfiguration = this;
+                            getterInvocations.add(name);
+                            return null;
+                        }
+                    }
+                }
+                if (fields.containsKey(name)) {
+                    return getField(name);
+                }
+            }
+            if (name.equals("sendMessage") && args.length == 1 &&
+                    Invokable.from(method).getParameters().get(0).getType().equals(PARTY_TYPE)) {
+                return sendMessage((String) args[0]);
+            }
+            if (name.equals("receiveMessage") && args.length == 2 &&
+                    Invokable.from(method).getParameters().get(0).getType().equals(PARTY_TYPE) &&
+                    Invokable.from(method).getParameters().get(1).getType().equals(MESSAGE_TYPE)) {
+                return receiveMessage((String) args[0], (Map<String, ?>) args[1]);
+            }
+            return invokeDefaultMethod(method, args);
+        }
+
+        private Object getField(String name) {
+            return fields.get(name).getMainVersion();
+        }
+
+        private void putFieldIfAbsent(String name, FieldVersions.Source source, Object value) {
+            var versions = fields.computeIfAbsent(name, k -> new FieldVersions<>(source, value));
+            versions.getAlternativeVersionMap().putIfAbsent(source, value);
+        }
+
+        private void computeFieldIfAbsent(String name, FieldVersions.Source source, Supplier<Object> valueSupplier) {
+            var versions = fields.computeIfAbsent(name, k -> new FieldVersions<>(source, valueSupplier.get()));
+            versions.getAlternativeVersionMap().computeIfAbsent(source, k -> valueSupplier.get());
+        }
+
+        private void updateDependentFields(Set<String> newFields) {
+            while (!newFields.isEmpty()) {
+                var addedFields = new HashSet<String>();
+                var notAddedFields = Sets.difference(fields.keySet(), addedFields);
+                for (var name : dependencyMultimap.keySet()) {
+                    var dependenciesList = dependencyMultimap.get(name);
+                    for (int i = 0; i < dependenciesList.size(); i++) {
+                        var dependencies = dependenciesList.get(i);
+                        if (notAddedFields.containsAll(dependencies) && dependencies.stream().anyMatch(newFields::contains)) {
+                            if (actorMap.containsKey(name)) {
+                                if (actorMap.get(name).equals(owner)) {
+                                    pendingActions.add(name);
+                                }
+                            } else if (updateDependentField(name, i)) {
+                                addedFields.add(name);
+                            }
+                        }
+                    }
+                }
+                newFields = addedFields;
+            }
+        }
+
+        private boolean updateDependentField(String name, int index) {
+            var source = new FieldVersions.DependencySource(index, dependencyMultimap.get(name).get(index));
+            var isUpdated = new AtomicBoolean();
+            computeFieldIfAbsent(name, source, () -> {
+                try {
+                    isUpdated.set(true);
+                    var method = clazz.getMethod(name);
+                    return IndexInjector.call(() -> invokeDefaultMethod(method), index);
+                } catch (Error | RuntimeException e) {
+                    throw e;
+                } catch (Throwable t) {
+                    throw new RuntimeException(t);
+                }
+            });
+            return isUpdated.get() && fields.get(name).getMainVersionSource().equals(source);
+        }
+
+        private Object invokeDefaultMethod(Method method, Object... args) throws Throwable {
+            var lookup = MethodHandles.privateLookupIn(method.getDeclaringClass(), MethodHandles.lookup());
+            var methodType = MethodType.methodType(method.getReturnType(), method.getParameterTypes());
+            var handle = Modifier.isStatic(method.getModifiers())
+                    ? lookup.findStatic(method.getDeclaringClass(), method.getName(), methodType)
+                    : lookup.findSpecial(method.getDeclaringClass(), method.getName(), methodType, method.getDeclaringClass());
+            return handle.bindTo(proxy).invokeWithArguments(args);
+        }
+
+        private Map<String, ?> sendMessage(String recipient) {
+            var set = Sets.union(fields.keySet(), Set.of(recipient));
+            var recipientSource = new FieldVersions.ExternalSource(recipient);
+            synchronized (recipientMultimap) {
+                var result = fields.keySet().stream()
+                        .filter(name -> !recipientMultimap.containsEntry(name, recipient))
+                        .filter(name -> !fields.get(name).getAllVersionMap().containsKey(recipientSource))
+                        .filter(name -> accessConditionMap.get(name).test(set))
+                        .collect(ImmutableMap.toImmutableMap(name -> name, this::getField));
+
+                result.keySet().forEach(id -> recipientMultimap.put(id, recipient));
+                return result;
+            }
+        }
+
+        private Void receiveMessage(String sender, Map<String, ?> message) {
+            var source = new FieldVersions.ExternalSource(sender);
+            message.forEach((name, value) -> putFieldIfAbsent(name, source, value));
+            updateDependentFields(ImmutableSet.copyOf(message.keySet()));
+            return null;
+        }
+
+        private void setEventValue(String name, Object value) {
+            Preconditions.checkArgument(owner.equals(eventObserverMap.get(name)),
+                    "Event '%s' not observable by the state owner '%s'", name, owner);
+            Preconditions.checkArgument(!fields.containsKey(name), "Event '%s' has already fired", name);
+            var source = new FieldVersions.ExternalSource(owner);
+            putFieldIfAbsent(name, source, value);
+        }
+
+        @Override
+        public String toString() {
+            var sb = new StringBuilder(clazz.getSimpleName()).append(' ');
+            sb.append("{\n  owner = ").append(owner);
+            sb.append(",\n  pendingActions = ").append(pendingActions);
+            sb.append(",\n  fields = {");
+            var len = sb.length();
+            fields.forEach((name, versions) ->
+                    sb.append(len == sb.length() ? "" : ",").append("\n    ").append(name).append(" = ").append(versions)
+                            .append(" (sent to ").append(recipientMultimap.get(name)).append(")")
+            );
+            return sb.append("\n  }\n}").toString();
+        }
     }
 }
--- finance/src/test/java/misq/finance/contract/sharedState/SharedStateFactoryTest.java
+++ finance/src/test/java/misq/finance/contract/sharedState/SharedStateFactoryTest.java
@@ -1,7 +1,14 @@
 package misq.finance.contract.sharedState;
 
+import misq.chain.Bitcoind;
 import misq.finance.swap.contract.multiSig.MultiSig;
 import org.junit.Test;
+
+import java.util.Map;
+
+import static misq.finance.contract.SecurityProvider.Unit.UNIT;
+import static misq.finance.contract.sharedState.SharedStateFactory.fireEvent;
+import static misq.finance.contract.sharedState.SharedStateFactory.when;
 
 public class SharedStateFactoryTest {
     @Test
@@ -24,5 +31,89 @@ public class SharedStateFactoryTest {
         System.out.println();
         factory.getAccessConditionMap().forEach((id, cond) -> System.out.println(id + " -> " + cond));
         System.out.println();
+
+        var wallet = new Bitcoind();
+        var makerState = factory.create("maker", state -> {
+            when(state.makerWallet()).thenReturn(wallet);
+            when(state.makerEscrowPublicKey()).thenReturn("makerPubKey");
+            when(state.makerPayoutAddress()).thenReturn("makerPayoutAddress");
+        });
+        var takerState = factory.create("taker", state -> {
+            when(state.takerWallet()).thenReturn(wallet);
+            when(state.takerEscrowPublicKey()).thenReturn("takerPubKey");
+            when(state.takerPayoutAddress()).thenReturn("takerPayoutAddress");
+        });
+
+        Map<String, ?> message;
+
+        System.out.println(makerState);
+        System.out.println();
+
+        System.out.println("GOT MESSAGE FROM maker TO taker: " + toString(message = makerState.sendMessage("taker")));
+        System.out.println();
+        takerState.receiveMessage("maker", message);
+
+        System.out.println(takerState);
+        System.out.println();
+
+        System.out.println("GOT MESSAGE FROM taker TO maker: " + toString(message = takerState.sendMessage("maker")));
+        System.out.println();
+        makerState.receiveMessage("taker", message);
+
+        System.out.println(makerState);
+        System.out.println();
+
+        System.out.println("GOT MESSAGE FROM maker TO taker: " + toString(message = makerState.sendMessage("taker")));
+        System.out.println();
+        takerState.receiveMessage("maker", message);
+
+        System.out.println(takerState);
+        System.out.println();
+
+        System.out.println("FIRING EVENTS: takerSeesDepositTxInMempool, makerSeesDepositTxInMempool, makerStartsCountercurrencyPayment");
+        System.out.println();
+        fireEvent(takerState::takerSeesDepositTxInMempool, "publishedDepositTx");
+        fireEvent(makerState::makerSeesDepositTxInMempool, "publishedDepositTx");
+        fireEvent(makerState::makerStartsCountercurrencyPayment, UNIT);
+
+        System.out.println(makerState);
+        System.out.println();
+
+        System.out.println("GOT MESSAGE FROM maker TO taker: " + toString(message = makerState.sendMessage("taker")));
+        System.out.println();
+        takerState.receiveMessage("maker", message);
+
+        System.out.println("FIRING EVENTS: takerConfirmsCountercurrencyPayment, takerSeesPayoutTxInMempool, makerSeesPayoutTxInMempool");
+        System.out.println();
+        fireEvent(takerState::takerConfirmsCountercurrencyPayment, UNIT);
+        fireEvent(takerState::takerSeesPayoutTxInMempool, "publishedPayoutTx");
+        fireEvent(makerState::makerSeesPayoutTxInMempool, "publishedPayoutTx");
+
+        System.out.println(takerState);
+        System.out.println();
+
+        System.out.println("GOT MESSAGE FROM taker TO maker: " + toString(message = takerState.sendMessage("maker")));
+        System.out.println();
+        makerState.receiveMessage("taker", message);
+
+        System.out.println(makerState);
+        System.out.println();
+
+        System.out.println("GOT MESSAGE FROM maker TO taker: " + toString(message = makerState.sendMessage("taker")));
+        System.out.println();
+        takerState.receiveMessage("maker", message);
+
+        System.out.println(makerState.makerDepositTxInputs());
+        System.out.println();
+//        System.out.println(makerState.depositTx());
+    }
+
+    private static String toString(Map<String, ?> message) {
+        var sb = new StringBuilder("{");
+        var len = sb.length();
+        message.forEach((name, value) ->
+                sb.append(len == sb.length() ? "" : ",").append("\n  ").append(name).append(" = ").append(value)
+        );
+        return sb.append("\n}").toString();
     }
 }
